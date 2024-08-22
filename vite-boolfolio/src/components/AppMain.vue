0ee4c074714de28df30f1ae605a7b93aa6ef0d0a
Benedetta-Barbone <barbone1912@gmail.com>
2024-08-21 15:53:54 -1200

    milestone 4

--- vite-boolfolio/src/components/AppMain.vue
+++ vite-boolfolio/src/components/AppMain.vue
@@ -1,9 +1,40 @@
 <script>
+import axios from 'axios';
 
+export default {
+    data() {
+        return{
+            posts: [],
+        }
+    },
+    methods:{
+        getPosts(){
+            axios.get('http://127.0.0.1:8000/api/posts', {
+                params:{
+
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results.data);
+                this.posts = response.data.results.data;
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+        }
+    },
+    created(){
+        this.getPosts();
+    }
+}
 </script>
 
 <template>
-
+<ul>
+    <li v-for="post in posts">
+        {{ post.title }}
+    </li>
+</ul>
 </template>
 
 <style>
